168a9c08ff875127ddffad03fcab23b45fd3150a
Jaime de Greiff <alegreiff@gmail.com>
2018-06-18 20:09:27 -0500

    gruppie

--- polla/src/components/PosFulbol.vue
+++ polla/src/components/PosFulbol.vue
@@ -10,7 +10,14 @@
         <v-flex xs3>
             adasdas
         </v-flex>
-        <v-flex xs8>
+        <v-flex xs5>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto rem dolorem quaerat quidem placeat velit sed natus at. A vel, laudantium iure pariatur fugiat cumque sapiente delectus! Dignissimos, nostrum rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptas saepe, temporibus labore aliquam in consequuntur quisquam fugiat! Provident accusamus, suscipit assumenda amet dolore architecto fugit eveniet officia ad deleniti.
+            Nobis aliquam odit dignissimos quasi quas quo, numquam doloremque aliquid, est placeat molestias iusto voluptatem aperiam reprehenderit? Corporis vero tempore ipsam? Labore veritatis doloremque tempore repellendus, pariatur dolor autem. Suscipit?
+            Recusandae possimus beatae, autem, magni error nostrum nesciunt sapiente atque eveniet corporis dolorum? Ipsum omnis alias quisquam, natus non vero illo eius ab molestiae suscipit numquam delectus laborum iste expedita!
+            Similique, culpa laudantium molestias nemo dolor, atque porro veniam reprehenderit natus ipsa alias doloremque ipsam. Eveniet placeat alias iure quaerat doloribus voluptates debitis! Laborum, molestiae delectus. Nostrum, iusto ipsa. Voluptatibus.
+            Earum maiores numquam eligendi ea architecto consequuntur esse asperiores similique quia, officiis reiciendis? Enim voluptas repudiandae mollitia dolor doloribus numquam eveniet culpa? Sunt autem repellendus vitae corporis consectetur adipisci vel.
+        </v-flex>
+        <v-flex xs4>
             <div class="tablagrupo">
                 <table class="tabla_np elevation-24">
                     <thead>
@@ -73,7 +80,7 @@
                             <td>{{ eq.pp }}</td>
                             <td>{{ eq.gf }}</td>
                             <td>{{ eq.gc }}</td>
-                            <td>{{ parseInt(eq.gf) + parseInt(eq.gc) }}</td>
+                            <td>{{ parseInt(eq.gf) - parseInt(eq.gc) }}</td>
                             <td>{{ parseInt(eq.pg) * 3 + parseInt(eq.pe) }}</td>
                         </tr>
                     </tbody>
