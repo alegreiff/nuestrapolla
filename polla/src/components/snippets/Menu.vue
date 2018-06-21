1b9b5bca10e7cfe41b39773b0b194f2f33f6834c
Jaime de Greiff <alegreiff@gmail.com>
2018-06-21 07:43:25 -0500

    Posiciones X grupos

--- polla/src/components/snippets/Menu.vue
+++ polla/src/components/snippets/Menu.vue
@@ -8,6 +8,10 @@
             <v-list-tile-action><v-icon>more_horiz</v-icon></v-list-tile-action>
             <v-list-tile-content><v-list-tile-title>Posiciones</v-list-tile-title></v-list-tile-content>
         </v-list-tile>
+        <v-list-tile :to="{path: '/grupos'}" > 
+            <v-list-tile-action><v-icon>more_horiz</v-icon></v-list-tile-action>
+            <v-list-tile-content><v-list-tile-title>Equipos</v-list-tile-title></v-list-tile-content>
+        </v-list-tile>
         <v-list-tile :to="{path: '/pdfs'}">
             <v-list-tile-action><v-icon>more_horiz</v-icon></v-list-tile-action>
             <v-list-tile-content><v-list-tile-title>PDF Mis pronósticos</v-list-tile-title></v-list-tile-content>
