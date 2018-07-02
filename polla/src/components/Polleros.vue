f6f7a65b09da9733c064c63ba57ba8a7a9192209
Jaime de Greiff <alegreiff@gmail.com>
2018-07-01 21:22:43 -0500

    PTS OCTAVOS

--- polla/src/components/Polleros.vue
+++ polla/src/components/Polleros.vue
@@ -182,9 +182,9 @@ Ganador del premio a líder de la fase de grupos $936.000 (Premio definitivo)
 
             
             <pre> {{ datosAdicionalesPollero.genero }} </pre>  
-            <pre>Puntos en fase de grupos: {{ datosAdicionalesPollero.grupos }}</pre>
-            <pre>Puntos en fase de octavos: {{ datosAdicionalesPollero.octavos }}</pre>
-            <pre>Puntos en fase de cuartos: {{ datosAdicionalesPollero.cuartos }}</pre>
+            <pre>grupos: {{ datosOtrosPolleros.grupos }}</pre>
+            <pre>octavos: {{ datosOtrosPolleros.octavos }}</pre>
+            <pre>cuartos: {{ datosOtrosPolleros.cuartos }}</pre>
             
            
             </v-flex>
